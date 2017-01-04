d56212ba0e779020dc1d2471348193e43ad733d1
ideas4u <xiewygz@m.scnu.edu.cn>
2017-01-04 13:19:15 +0800

    add the weixinInterface.py file to the repo

---
+++ weixinInterface.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import hashlib
+import web
+import lxml
+import time
+import os
+import urllib2, json
+from lxml import etree
+
+class WeixinInterface:
+	def __init__(self):
+		self.app_root = os.path.dirname(__file__)
+		self.templates_root = os.path.join(self.app_root,'templates')
+		self.render = web.template.render(self.templates_root)
+
+	def GET(self):
+		#get the input paramater
+		data = web.input()
+		signature = data.signature
+		timestamp = data.timestamp
+		nonce = data.nonce
+		echostr = data.echostr
+
+		# your token
+		token = "xxx" # here change to ur wechar platform token
+		# dict sort
+		list=[token,timestamp,nonce]
+		list.sort()
+		sha1 = hashlib.sha1()
+		map(sha1.update,list)
+		hashcode = sha1.hexdigest()
+
+		# sha1 recode athgridem
+
+		# if the request is from wechar ,then reply echostr
+		if hashcode == signature:
+			return echostr
+
